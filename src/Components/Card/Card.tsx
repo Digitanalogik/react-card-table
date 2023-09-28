837149f9246a6fc395a3882e66a3f7a8a01138f3
Tatu Soininen <taatusti@gmail.com>
2023-09-28 14:30:07 +0300

    Refactor Card

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -8,17 +8,34 @@ interface CardProps {
 }
 
 const Card = ({ type, value, title, description }: CardProps): JSX.Element => {
-  return (
-  <div className="card">
-    <div className={`card-content ${type}`}>
-      {type === 'numeric' ? (
+
+  const renderCard = () => {
+    if (type === 'numeric') {
+      return renderNumeric();
+    } else {
+      return renderSpecial();
+    }
+  };
+
+  const renderNumeric = () => {
+    return (
+      <div className="card">
         <span className="card-number">{value}</span>
-      ) : (
-        <span className="card-text">{description}</span>
-      )}
-    </div>
-    <div className="card-title">{title}</div>
-  </div>  );
+        <div className="card-title">{title}</div>
+      </div>  
+    );
+  };
+
+  const renderSpecial = () => {
+    return (
+      <div className="card">
+        <span className="card-text">{title}</span>
+        <div className="card-title">{description}</div>
+      </div>  
+    );
+  };
+
+  return renderCard();
 }
 
 export default Card
