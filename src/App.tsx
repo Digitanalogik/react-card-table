aac583394c7b6754b08e0cee81cbb74c943f3ac2
Tatu Soininen <taatusti@gmail.com>
2023-09-28 14:02:58 +0300

    Include object identifiers in array

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 const App = (): JSX.Element => {
 
   const CARDS = [
-    { "number": 1, "title": "Card 1" },
-    { "number": 2, "title": "Card 2" },
-    { "number": 3, "title": "Card 3" },
-    { "number": 4, "title": "Card 4" },
-    { "number": 5, "title": "Card 5" },
-    { "number": 6, "title": "Card 6" },
-    { "number": 7, "title": "Card 7" },
-    { "number": 8, "title": "Card 8" },
-    { "number": 9, "title": "Card 9" },
-    { "number": 10, "title": "Card 10" }
+    { "id": 1, "number": 1, "title": "Card 1" },
+    { "id": 2, "number": 2, "title": "Card 2" },
+    { "id": 3, "number": 3, "title": "Card 3" },
+    { "id": 4, "number": 4, "title": "Card 4" },
+    { "id": 5, "number": 5, "title": "Card 5" },
+    { "id": 6, "number": 6, "title": "Card 6" },
+    { "id": 7, "number": 7, "title": "Card 7" },
+    { "id": 8, "number": 8, "title": "Card 8" },
+    { "id": 9, "number": 9, "title": "Card 9" },
+    { "id": 10, "number": 10, "title": "Card 10" }
   ]
 
   return (
