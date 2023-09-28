8532b552aa07a3bdc4a739ca871e17214ee12603
Tatu Soininen <taatusti@gmail.com>
2023-09-28 16:52:34 +0300

    Separate card rendering logic

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -1,22 +1,46 @@
-import Card from '../Card/Card';
+import { ReactElement } from 'react';
+import Card, { CardProps } from '../Card/Card';
 import './Table.css';
 
 interface TableProps {
-  cards?:  any;
+  cards?:  Array<CardProps>;
 }
 
-const Table = ({ cards }: TableProps) => {
+const Table = ({ cards }: TableProps): ReactElement => {
+
+  const numericCards = cards?.filter(card => card.type === 'numeric');
+  const specialCards = cards?.filter(card => card.type === 'special');
+
+  const renderNumericCards = (): ReactElement => {
+    return renderCards(numericCards, "numeric");
+  };
+
+  const renderSpecialCards = (): ReactElement => {
+    return renderCards(specialCards, "special");
+  };
+
+  const renderCards = (cards: any, sectionName: string): ReactElement => {
+    return (
+      <div className={`cards ${sectionName}`}>
+        {cards.map((card: any) => (
+          <Card key={card.id} 
+            type={card.type}
+            value={card.value}
+            title={card.title}
+            description={card?.description}
+            image={card?.image}
+          />
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div className="table">
-      {cards.map((card: any, index: number) => (
-        <Card key={card.id} 
-          type={card.type}
-          value={card.value}
-          title={card.title}
-          description={card?.description}
-          image={card?.image}
-        />
-      ))}
+    <div className='table'>
+      <div className="cards">
+        {renderNumericCards()}
+        {renderSpecialCards()}
+      </div>
     </div>
   );
 }
