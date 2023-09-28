0cfa1c7212d2f13f815200b3dc68495a53dcf5e5
Tatu Soininen <taatusti@gmail.com>
2023-09-28 14:18:31 +0300

    Support numeric and special cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,17 +1,24 @@
 import './Card.css';
 
 interface CardProps {
-  number: number;
+  type: string;
+  value: number;
   title: string;
+  description?: string
 }
 
-const Card = ({ number, title }: CardProps): JSX.Element => {
+const Card = ({ type, value, title, description }: CardProps): JSX.Element => {
   return (
-    <div className='card'>
-      <div className='card-number'>{number}</div>
-      <div className='card-title'>{title}</div>
+  <div className="card">
+    <div className={`card-content ${type}`}>
+      {type === 'numeric' ? (
+        <span className="card-number">{value}</span>
+      ) : (
+        <span className="card-text">{description}</span>
+      )}
     </div>
-  );
+    <div className="card-title">{title}</div>
+  </div>  );
 }
 
 export default Card
--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -9,7 +9,7 @@ const Table = ({ cards }: TableProps) => {
   return (
     <div className="table">
       {cards.map((card: any, index: number) => (
-        <Card key={card.id} number={card.number} title={card.title} />
+        <Card key={card.id} type={card.type} value={card.value} title={card.title} description={card?.description} />
       ))}
     </div>
   );
