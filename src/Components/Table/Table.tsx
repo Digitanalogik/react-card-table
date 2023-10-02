fdb55d848ae9085bd6f0f17e12bf83117949bd6c
Tatu Soininen <taatusti@gmail.com>
2023-10-02 14:14:19 +0300

    Send selected card value/title as HTTP POST body

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -1,6 +1,5 @@
 import { ReactElement, useState } from 'react';
 import Card, { CardProps } from '../Card/Card';
-import ICard from '../../Interfaces/Card';
 import { post } from '../../Services/ApiClient';
 import './Table.css';
 
@@ -45,17 +44,25 @@ const Table = ({ cards }: TableProps): ReactElement => {
     return id === selected;
   };
 
-  const select = async (card: ICard) => {
+  const select = async (card: any) => {
     console.log("Selected:", card);
     setSelected(card.id);
+
+    const postData = {
+      playerName: 'Default',
+      cardValue: card?.value,
+      cardTitle: card?.title
+    };
+
+    console.log("Post Data:", postData);
+
     try {
-      const response = await post('/card', JSON.stringify(card));
+      const response = await post('/card', JSON.stringify(postData));
       console.log('Response from API:', response);
     } catch (error) {
       console.error('Error during API request:', error);
     }    
   };
-
 
   return (
     <div className='table'>
