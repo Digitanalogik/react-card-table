b4724882ee5d8b576d9086405b35c961fb87281a
Tatu Soininen <taatusti@gmail.com>
2023-10-02 11:34:13 +0300

    Helper function to test API connection

--- src/Services/ApiClient.ts
+++ src/Services/ApiClient.ts
@@ -4,13 +4,22 @@ export const apiClient: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const apiTest = async (): Promise<any> => {
+  console.log("API Client - Test");
+  const httpClient = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+  });
+  const response = await httpClient.get('/posts');
+  console.log("API Test - response:", response);
+}
+
 export const get = async (api: string): Promise<any> => {
   console.log("API Client - GET", api);
   try {
     const response = await apiClient.get(api);
     return response.data;
   } catch (error) {
-    console.error('Error posting data:', error);
+    console.error('Error getting data:', error);
     throw error;
   }
 };
