7a30e8d8fe007a3ae0d1cd1ab3891e0f24a2f7ac
Tatu Soininen <taatusti@gmail.com>
2023-10-03 19:03:24 +0300

    Use string identifiers for DynamoDB compatibility

--- src/Model/ScrumGame.ts
+++ src/Model/ScrumGame.ts
@@ -28,15 +28,15 @@ export const CARDS = [
 ];
 
 // ToDo: get players from backend - this is just test data
-export const PLAYERS = [
-  { id: 1, name: 'Alice' },
-  { id: 2, name: 'Bob' },
-  { id: 3, name: 'Charlie' },
-  { id: 4, name: 'David' },
-  { id: 5, name: 'Eva' },
-  { id: 6, name: 'Fiona' },
-  { id: 7, name: 'George' },
-  { id: 8, name: 'Hannah' },
-  { id: 9, name: 'Igor' },
-  { id: 10, name: 'Jane' }
+export const PLAYERS: ScrumPokerPlayer[]  = [
+  { id: "1", name: 'Alice' },
+  { id: "2", name: 'Bob' },
+  { id: "3", name: 'Charlie' },
+  { id: "4", name: 'David' },
+  { id: "5", name: 'Eva' },
+  { id: "6", name: 'Fiona' },
+  { id: "7", name: 'George' },
+  { id: "8", name: 'Hannah' },
+  { id: "9", name: 'Igor' },
+  { id: "10", name: 'Jane' }
 ];
