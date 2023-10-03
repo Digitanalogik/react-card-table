a1bdc9b33335203194ed50bc2c58ba1e83b87248
Tatu Soininen <taatusti@gmail.com>
2023-10-03 18:59:48 +0300

    Use custom data type instead of string to represent player

--- src/Context/GameContext.tsx
+++ src/Context/GameContext.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, createContext, useContext, useState, useMemo } from 'react';
+import { ScrumPokerPlayer, DEFAULT_PLAYER } from '../Model/ScrumGame';
 
 type GameContextType = {
   isLogged: boolean;
@@ -7,8 +8,8 @@ type GameContextType = {
   room: string;
   setRoom: (room: string) => void;
 
-  player: string;
-  setPlayer: (player: string) => void;
+  player: ScrumPokerPlayer;
+  setPlayer: (player: ScrumPokerPlayer) => void;
 
   secret: string;
   setSecret: (secret: string) => void;
@@ -34,7 +35,7 @@ const useGameContext = (): GameContextType => {
 const GameContextProvider = ({ children }: GameContextProps): ReactElement => {
   const [isLogged, setIsLogged] = useState<boolean>(false);
   const [room, setRoom] = useState<string>("Default Room");
-  const [player, setPlayer] = useState<string>("Default Player");
+  const [player, setPlayer] = useState<ScrumPokerPlayer>(DEFAULT_PLAYER);
   const [secret, setSecret] = useState<string>("");
 
   const gameContextProviderValue = useMemo(() => (
