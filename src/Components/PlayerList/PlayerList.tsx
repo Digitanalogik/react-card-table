aa327f490474a6c9e7dc10882efc91762b0f6e9a
Tatu Soininen <taatusti@gmail.com>
2023-10-03 22:37:39 +0300

    Use interval polling to fetch data frequently from backend API

--- src/Components/PlayerList/PlayerList.tsx
+++ src/Components/PlayerList/PlayerList.tsx
@@ -1,24 +1,55 @@
 // UserList.tsx
-import  { ReactElement } from 'react';
-import { ScrumPokerPlayer } from '../../Model/ScrumGame';
+import { ReactElement, useEffect, useState } from 'react';
 import { useGameContext } from '../../Context/GameContext';
+import { get } from '../../Services/ApiClient';
 import './PlayerList.css';
 
-interface UserListProps {
-  players:  ScrumPokerPlayer[];
-}
+const PlayerList = (): ReactElement => {
 
-const PlayerList = ({ players }: UserListProps): ReactElement => {
+  const pollingInterval = 10000; // Polling interval in milliseconds (10 seconds)
 
-  const { player } = useGameContext();
+  const { player, players, setPlayers } = useGameContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await get('/players');
+      setPlayers(response);
+
+      console.log("API response: ", response);
+    };
+
+    // Create a function to start the polling
+    const startPolling = () => {
+      fetchData();
+
+      // Set a timeout to call the startPolling function again after the polling interval
+      return setTimeout(() => {
+        startPolling();
+      }, pollingInterval);
+    };
+
+    // Start polling for backend API
+    const timeoutId = startPolling();
+
+    // Cleanup function to clear the timeout when the component is unmounted
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []); // Empty dependency array ensures the effect only runs once
+
+  // List all players
+  useEffect(() => {
+    console.group("Scrum Poker - Players");
+    players.forEach(p => console.log(p.name));
+    console.groupEnd();
+  }, [players]);
 
   // Always show current player on top of the list
   return (
     <div className='player-list'>
         <div key={player.id} className='player-name'>{player.name}</div>
 
-        {players.filter(p => p.name !== player.name)
-          .sort((a, b) => a.name.localeCompare(b.name)).map(p =>
+        {players.map(p =>
             <div key={p.id} className='player-name'>{p.name}</div>
         )}
     </div>
