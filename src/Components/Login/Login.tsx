c0c220efe4e6559fbb832095686e2255fc9e6b06
Tatu Soininen <taatusti@gmail.com>
2023-10-03 15:30:51 +0300

    Input room secret and login to backend

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,15 +1,36 @@
-import React, { ReactElement } from 'react';
-import './Login.css';
+import { ReactElement } from 'react';
 import { useGameContext } from '../../Context/GameContext';
+import { post } from '../../Services/ApiClient';
+import './Login.css';
 
 const Login = (): ReactElement => {
 
-  const { setIsLogged, player, setPlayer, room, setRoom } = useGameContext();
+  const { setIsLogged, player, setPlayer, room, setRoom, secret, setSecret } = useGameContext();
 
-  const enterRoom = () => {
-    setIsLogged(true);
+  const enterRoom = async () => {
+    // JSON data to send to the API as HTTP POST body payload
+    const data = {
+      playerName: player,
+      roomName: room,
+      roomSecret: secret
+    }
+
+    // Display debug information in the client browser
+    console.group('Scrum Poker - Enter Room');
+    console.log('Player: ', player);
+    console.log('Room: ', room);
+    console.log('Secret: ', secret);
+    console.groupEnd();
+
+    try {
+      const response = await post('/player', JSON.stringify(data));
+      console.log('Response from API:', response);
+      setIsLogged(true);
+    } catch (error) {
+      console.error('Error during API request:', error);
+      setIsLogged(false);
+    }
   }
-
 
   return (
     <div className='login'>
@@ -19,7 +40,7 @@ const Login = (): ReactElement => {
           id='player'
           className='player'
           aria-label='Player name'
-          placeholder='Enter player name'
+          placeholder='What is your name?'
           value={player}
           onChange={(event) => setPlayer(event.target.value)}
         />
@@ -30,7 +51,7 @@ const Login = (): ReactElement => {
           id='room'
           className='room'
           aria-label='Room name'
-          placeholder='Enter room name'
+          placeholder='Where do you want to play?'
           value={room}
           onChange={(event) => setRoom(event.target.value)}
           onKeyDown={async (event) => {
@@ -38,6 +59,26 @@ const Login = (): ReactElement => {
               enterRoom()
             } else if (event.key === 'Escape') {
               setRoom("");
+            }
+          }}
+        />
+    </div>
+    <div className='login-input'>
+      <span className='label'>Secret</span>
+      <input type="text"
+          id='secret'
+          className='secret'
+          aria-label='Room secret'
+          placeholder='Optional: room secret'
+          value={secret}
+          onChange={(event) => setSecret(event.target.value)}
+          onKeyDown={async (event) => {
+            if (event.key === 'Enter') {
+              if (window.confirm("Enter room " + room + " as " + player + "?")) {
+                enterRoom();
+              }
+            } else if (event.key === 'Escape') {
+              setSecret("");
             }
           }}
         />
