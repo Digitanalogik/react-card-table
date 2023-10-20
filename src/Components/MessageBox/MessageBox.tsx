fc5126573f856fa8a740c6c05ccd490d5f14fdb0
Tatu Soininen <taatusti@gmail.com>
2023-10-20 17:13:42 +0300

    Component for showing runtime information to user

---
+++ src/Components/MessageBox/MessageBox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InfoRow from "../InfoRow/InfoRow";
+import "./MessageBox.css";
+
+const MessageBox = () => {
+  return (
+    <div className="message-box">
+      <InfoRow data="Message Box" />
+    </div>
+  )
+};
+
+export default MessageBox;
