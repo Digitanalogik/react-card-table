f48efe02b1a59a207021140208038229bc4ca881
Tatu Soininen <taatusti@gmail.com>
2023-10-20 19:13:23 +0300

    Show WebSocket connection information in the message box

--- src/Components/MessageBox/MessageBox.tsx
+++ src/Components/MessageBox/MessageBox.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useWebSocketConnection } from "../../Context/WebSocketContext";
 import InfoRow from "../InfoRow/InfoRow";
 import "./MessageBox.css";
 
 const MessageBox = () => {
+
+  const { connectionStatus } = useWebSocketConnection();
+
   return (
     <div className="message-box">
       <InfoRow data="Message Box" />
+      <InfoRow data={connectionStatus} />
     </div>
   )
 };
